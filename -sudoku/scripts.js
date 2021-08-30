8dd6abaa47469c66f5ba5b7f5b554abab3af22ed
Chip-Mortensen <39780632+Chip-Mortensen@users.noreply.github.com>
2021-08-30 13:27:34 -0700

    .
    .

--- -sudoku/scripts.js
+++ -sudoku/scripts.js
@@ -1,192 +1,86 @@
-const gameBoard = new Array(9);
-for (let i = 0; i < 9; i++) {gameBoard[i] = new Array(9)};
-for (let x = 0; x < 9; x++) {
-    for (let y = 0; y < 9; y++) {
-        gameBoard[x][y] = 0;
+console.log(document.sudoku.generate('easy'));
+let board;
+
+const difficultyChoice = document.querySelectorAll('.info__difficulty-btn');
+for (let x = 0; x < difficultyChoice.length; x++) {
+    difficultyChoice[x].addEventListener('click', () => {
+        for (let y = 0; y < difficultyChoice.length; y++) {
+            difficultyChoice[y].classList.remove('btn-active')
+        }
+        difficultyChoice[x].classList.add('btn-active');
+    });
+}
+
+const generateBoard = () => {
+    let difficulty = document.querySelectorAll('.btn-active');
+    board = document.sudoku.generate(difficulty[0].innerHTML.toLowerCase());
+    updateValuesBoard();
+}
+
+const updateValuesBoard = () => {
+    let nodes = document.querySelectorAll('.node');
+    for (let x = 0; x < 81; x++) {
+        nodes[x].value = '';
+    }
+    for (let x = 0; x < 81; x++) {
+        if (board.substring(x, x+1) != '.') {
+            nodes[x].value = board.substring(x, x+1);
+        }
     }
 }
 
-let iterations = 20;
-
+const validateBoard = () => {
+    console.log(board);
+}
 
 const generateBoardHTML = () => {
-    const centerDiv = document.querySelector('#center-div');
-    const boardDiv = document.createElement('div');
-    boardDiv.setAttribute('id', 'board-div');
-    for (let w = 0; w < 3; w++) {
-        let squareRow = document.createElement('div');
-        squareRow.setAttribute('class', 'square-row');
-        for (let x = 0; x < 3; x++) {
-            let square = document.createElement('div');
-            square.setAttribute('class', 'square')
-            for (let y = 0; y < 3; y++) {
-                let nodeRow = document.createElement('div');
-                nodeRow.setAttribute('class', 'node-row')
-                for (let z = 0; z < 3; z++) {
-                    let node = document.createElement('input');
-                    node.setAttribute('maxlength', '1');
-                    node.setAttribute('class', 'node');
-                    node.addEventListener('dblclick',function(){node.value=''});
-                    nodeRow.appendChild(node);
-                }
-                square.appendChild(nodeRow);
-            }
-            squareRow.appendChild(square);
-        }
-        boardDiv.appendChild(squareRow);
-    }
-    centerDiv.appendChild(boardDiv);
-}
-
-const generateCell = (xIndex,yIndex,optionChoice,iter) => {
-    if (iter < 500) {
-        let nodeOptions = getNodeOptions(xIndex, yIndex);
-        if (nodeOptions.length == 0) {
-            let prevNode = prevNodeIndex(xIndex,yIndex);
-            printGameBoard();
-            console.log('Options: ' + nodeOptions);
-            console.log('Choice: Back Out');
-            console.log(' ');
-            gameBoard[prevNode[0]][prevNode[1]] = 0;
-            generateCell(prevNode[0], prevNode[1], optionChoice, ++iter);
-        } else {
-            let randIndex = Math.floor(Math.random() * (nodeOptions.length-1));
-            if (randIndex == optionChoice) {
-                if (nodeOptions.length > 1) {
-                    if (optionChoice == (nodeOptions.length-1)) {
-                        randIndex--;
-                    } else {
-                        randIndex++;
-                    }
-                }
-            }
-            gameBoard[xIndex][yIndex] = nodeOptions[randIndex];
-            let nextNode = nextNodeIndex(xIndex,yIndex);
-            printGameBoard();
-            console.log('Options: ' + nodeOptions);
-            console.log('Choice: ' + nodeOptions[randIndex]);
-            console.log(' ');
-            generateCell(nextNode[0], nextNode[1], optionChoice, ++iter);
-        }
-    } else {
-        clearGameBoard();
-        iterations--;
-        if (iterations > 0) {
-            generateCell(0,0,0,0);
-        }
-        printGameBoard();
-    }
-
-}
-
-const isEmptyNode = () => {
+    const board = document.querySelector('.board');
     for (let x = 0; x < 9; x++) {
         for (let y = 0; y < 9; y++) {
-            if (gameBoard[x][y] == 0) {
-                return true;
+            let node = document.createElement('input');
+            node.setAttribute('maxlength', '1');
+            node.className += 'node ';
+            node.className += `c${y} `;
+            node.className += `r${x} `;
+            if (x < 3) {
+                if (y < 3) {
+                    node.className += 's1';
+                } else if (y < 6) {
+                    node.className += 's2';
+                } else {
+                    node.className += 's3';
+                }
+            } else if (x < 6) {
+                if (y < 3) {
+                    node.className += 's4';
+                } else if (y < 6) {
+                    node.className += 's5';
+                } else {
+                    node.className += 's6';
+                }
+            } else if (x < 9) {
+                if (y < 3) {
+                    node.className += 's7';
+                } else if (y < 6) {
+                    node.className += 's8';
+                } else {
+                    node.className += 's9';
+                }
             }
-        }
-    }
-    return false;
-}
-
-const nextNodeIndex = (x, y) => {
-    if (y < 8 && x == 8) {
-        y++;
-        x = 0;
-    } else if (x < 8) {
-        x++;
-    }
-    let index = [x,y];
-    return index;
-}
-
-const prevNodeIndex = (x, y) => {
-    if (y > 0 && x == 0) {
-        y--;
-        x = 8;
-    } else if (x > 0) {
-        x--;
-    }
-    let index = [x,y];
-    return index;
-}
-
-const clearGameBoard = () => {
-    for (let x = 0; x < 9; x++) {
-        for (let y = 0; y < 9; y++) {
-            gameBoard[x][y] = 0;
+            
+            node.addEventListener('dblclick',function(){node.value=''});
+            node.addEventListener('keypress',
+            function(key) {
+                if(key.key == '1' || key.key == '2' || key.key == '3' || key.key == '4' || key.key == '5' || key.key == '6' || key.key == '7' || key.key == '8' || key.key == '9') {
+                    node.value = key.key;
+                    validateBoard();
+                }
+            });
+            board.appendChild(node);
         }
     }
 }
 
-const getNodeOptions = (x, y) => {
-    let options = [1,2,3,4,5,6,7,8,9];
-    let rowVals = usedValsRow(x);
-    let colVals = usedValsCol(y);
-    let boxVals = usedValsBox(x,y);
-    for (let x = 0; x < options.length; x++) {
-        for (let y = 0; y < rowVals.length; y++) {
-            if (options[x] == rowVals[y]) {
-                options[x] = null;
-            }
-        }
-    }
-    for (let x = 0; x < options.length; x++) {
-        for (let y = 0; y < colVals.length; y++) {
-            if (options[x] == colVals[y]) {
-                options[x] = null;
-            }
-        }
-    }
-    for (let x = 0; x < options.length; x++) {
-        for (let y = 0; y < boxVals.length; y++) {
-            if (options[x] == boxVals[y]) {
-                options[x] = null;
-            }
-        }
-    }
-    options = options.filter(x => x != null);
-    return options;
-}
 
-const printGameBoard = () => {
-    for (let x = 0; x < 9; x++) {
-        console.log(gameBoard[x][0] + "\t" + gameBoard[x][1] + "\t" + gameBoard[x][2] + "\t" + gameBoard[x][3] + "\t" + gameBoard[x][4] + "\t" + gameBoard[x][5] + "\t" + gameBoard[x][6] + "\t" + gameBoard[x][7] + "\t" + gameBoard[x][8])
-    }
-}
-
-const usedValsRow = (rowIndex) => {
-    let usedValues = [];
-    for (let i = 0; i < 9; i++) {
-        if (gameBoard[rowIndex][i] == null) {}
-        else {usedValues.push(gameBoard[rowIndex][i]);}
-    }
-    return usedValues;
-}
-
-const usedValsCol = (colIndex) => {
-    let usedValues = [];
-    for (let i = 0; i < 9; i++) {
-        if (gameBoard[i][colIndex] == null) {}
-        else {usedValues.push(gameBoard[i][colIndex]);}
-    }
-    return usedValues;
-}
-
-const usedValsBox = (rowIndex, colIndex) => {
-    let usedValues = [];
-    xAdjustment = rowIndex - (rowIndex % 3);
-    yAdjustment = colIndex - (colIndex % 3);
-    for (let x = (0+xAdjustment); x < (3+xAdjustment); x++) {
-        for (let y = (0+yAdjustment); y < (3+yAdjustment); y++) {
-            if (gameBoard[x][y] == null) {}
-            else {usedValues.push(gameBoard[x][y]);}
-        }
-    }
-    return usedValues;
-}
 
 generateBoardHTML();
-generateCell(0,0,0,0);
-printGameBoard();
